31bb1c421e51f76631ed866f3013db03c97d103b
ViniD88 <viniciusdeoliveira@sou.faccat.br>
2024-05-26 21:25:39 -0300

    Commit 26/05
     - UI finalizada
     - Criação da mecânica para o próximo cenário

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	public Animator anim;
+	public bool SubOK, MultOk, DivOk;
+    public RespostasAdição respostasAdição;
+
+    // Use this for initialization
+    void Start () {
+		anim = GetComponent<Animator> ();
+        respostasAdição = GameObject.FindObjectOfType<RespostasAdição>();
+        SubOK = false;
+        MultOk = false;
+        DivOk = false;
+    }
+
+	void OnTriggerEnter (Collider other) {
+		if (this.tag == "Gate")
+		{
+            anim.SetBool("DoorOpen", true);
+            anim.SetBool("DoorClose", false);
+        }
+
+        if (this.tag == "GateSub" && SubOK == true)
+        {
+            anim.SetBool("DoorOpen", true);
+            anim.SetBool("DoorClose", false);
+        }
+
+	}
+
+	void OnTriggerExit (Collider other) {
+        if (this.tag == "Gate")
+        {
+            anim.SetBool("DoorOpen", false);
+            anim.SetBool("DoorClose", true);
+        }
+
+        if (this.tag == "GateSub" && SubOK == true)
+        {
+            anim.SetBool("DoorOpen", false);
+            anim.SetBool("DoorClose", true);
+        }
+    }
+
+	void Update () {
+		if (respostasAdição.questoesCertas.Count == 5) {
+			SubOK = true;
+        }
+	}
+}
